b31e9477b9abd6d285d5946d338d9763f5755167
Farid Coronel <faridce14@gmail.com>
2025-05-01 00:38:32 -0500

    feat(logo): add logo api service client.

---
+++ src/app/shared/services/logo-api.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {environment} from '../../../environments/environment';
+import {Source} from '../../news/model/source.entity';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LogoApiService {
+  baseUrl = environment.logoProviderApiBaseUrl;
+  constructor() { }
+
+  geturlToLogo(source: Source) {
+    return `${this.baseUrl}/${new URL(source.url).hostname}`;
+  }
+}
