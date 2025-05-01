18af1595b46f02b36a8c564606b553ee5e29b415
Farid Coronel <faridce14@gmail.com>
2025-05-01 00:30:26 -0500

    chore: add environment variables.

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  newsProviderApiBaseUrl: 'https://newsapi.org/v2',
+  newsProviderNewsEndpointPath: '/top-headlines',
+  newsProviderSourcesEndPoitnPath: '/top-headlines/sources',
+  newsProviderApiKey: '2bbc0f03d5b341e094e3398595f873c5',
+  logoProviderApiBaseUrl: 'https://logo.clearbit.com',
+};
